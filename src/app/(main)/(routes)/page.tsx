c4b7c7876ce3f1152653829f87563952d502b219
Le Anh Duy <duyleanh2404@gmail.com>
2025-07-18 10:11:03 +0700

    init(project): set up initial project structure

---
+++ src/app/(main)/(routes)/page.tsx
@@ -0,0 +1,12 @@
+import { UserButton } from '@clerk/nextjs';
+
+import { ModeToggle } from '@/components/providers/mode-toggle';
+
+export default function Home() {
+  return (
+    <div>
+      <UserButton />
+      <ModeToggle />
+    </div>
+  );
+}
